47ae347165511191db8e7d2030165d83630a0824
Linda Haviv <Linda.Haviv@foxnews.com>
2019-11-29 18:08:33 -0500

    add initial alexa skill code

---
+++ questions.js
@@ -0,0 +1,220 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'Who was the first female to receive a PhD in computer science?': [
+        'Sister Mary Kenneth Keller',
+        'Grace Murray Hopper',
+        'Hedy Lamarr',
+        'Margaret Hamilton',
+      ],
+    },
+    {
+      'Which woman in tech helped form gaming company Sierra On-Line?': [
+        'Roberta Williams',
+        'Jeri Ellsworth',
+        'Ada Lovelace',
+        'Sheryl Sandberg',
+      ],
+    },
+    {
+      'Which woman in tech held a patent on a secret communication system?': [
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Grace Murray Hopper',
+        'Radia Perlman',
+      ],
+    },
+    {
+      'Which woman is considered the first female video game designer?': [
+        'Carol Shaw',
+        'Katherine Johnson',
+        'Grace Murray Hopper',
+        'Beatrice Helen Worsley',
+      ],
+    },
+    {
+      'Which woman in tech developed the algorithm behind the Spanning Tree Protocol?': [
+        'Radia Perlman',
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Sister Mary Kenneth Keller',
+      ],
+    },
+    {
+      'What\’s the name of the movie about the ladies computing in World War Two?': [
+        'Top Secret Rosies',
+        'Hidden Figures',
+        'Ghostbusters',
+        'Contact',
+      ],
+    },
+    {
+      'Which woman in tech is known for her hardware work in augmented reality?': [
+        'Jeri Ellsworth ',
+        'Marissa Mayer',
+        'Roberta Williams',
+        'Isabel Drost-Fromm',
+      ],
+    },
+    {
+      'Which female video game designer worked on River Raid and Super Breakout?': [
+        'Carol Shaw',
+        'Marissa Mayer',
+        'Radia Perlman',
+        'Grace Murray Hopper',
+      ],
+    },
+    {
+      'Which woman was the lead software engineer on Project Apollo??': [
+        'Margaret Hamilton',
+        'Sheryl Sandberg',
+        'Susan Wojcicki',
+        'Meg Whitman',
+      ],
+    },
+    {
+      'Ada Lovelace is often described as being the world’s first?': [
+        'Computer programmer',
+        'Economist',
+        'Mathematician',
+        'Engineer',
+      ],
+    },
+    {
+      'Which woman was a mathematician whose calculations of orbital mechanics as a NASA employee were critical to the success of the first and subsequent U.S. crewed spaceflights?': [
+        'Katherine Johnson',
+        'Grace Murray Hopper',
+        'Radia Perlman',
+        'Carol Shaw',
+      ],
+    },
+    {
+      'Which was one of the first women to study math, astronomy and philosophy': [
+        'Hypatia',
+        'Margaret Hamilton',
+        'Grace Murray Hopper',
+        'Meg Whitman',
+      ],
+    },
+    {
+      'Who gets credited with having found the first bug?': [
+        'Grace Murray Hopper',
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Sister Mary Kenneth Keller',
+      ],
+    },
+  ],
+  QUESTIONS_EN_US: [
+    {
+      'Who was the first female to receive a PhD in computer science?': [
+        'Sister Mary Kenneth Keller',
+        'Grace Murray Hopper',
+        'Hedy Lamarr',
+        'Margaret Hamilton',
+      ],
+    },
+    {
+      'Which woman in tech helped form gaming company Sierra On-Line?': [
+        'Roberta Williams',
+        'Jeri Ellsworth',
+        'Ada Lovelace',
+        'Sheryl Sandberg',
+      ],
+    },
+    {
+      'Which woman in tech held a patent on a secret communication system?': [
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Grace Murray Hopper',
+        'Radia Perlman',
+      ],
+    },
+    {
+      'Which woman is considered the first female video game designer?': [
+        'Carol Shaw',
+        'Katherine Johnson',
+        'Grace Murray Hopper',
+        'Beatrice Helen Worsley',
+      ],
+    },
+    {
+      'Which woman in tech developed the algorithm behind the Spanning Tree Protocol?': [
+        'Radia Perlman',
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Sister Mary Kenneth Keller',
+      ],
+    },
+    {
+      'What\’s the name of the movie about the ladies computing in World War Two?': [
+        'Top Secret Rosies',
+        'Hidden Figures',
+        'Ghostbusters',
+        'Contact',
+      ],
+    },
+    {
+      'Which woman in tech is known for her hardware work in augmented reality?': [
+        'Jeri Ellsworth ',
+        'Marissa Mayer',
+        'Roberta Williams',
+        'Isabel Drost-Fromm',
+      ],
+    },
+    {
+      'Which female video game designer worked on River Raid and Super Breakout?': [
+        'Carol Shaw',
+        'Marissa Mayer',
+        'Radia Perlman',
+        'Grace Murray Hopper',
+      ],
+    },
+    {
+      'Which woman was the lead software engineer on Project Apollo??': [
+        'Margaret Hamilton',
+        'Sheryl Sandberg',
+        'Susan Wojcicki',
+        'Meg Whitman',
+      ],
+    },
+    {
+      'Ada Lovelace is often described as being the world’s first?': [
+        'Computer programmer',
+        'Economist',
+        'Mathematician',
+        'Engineer',
+      ],
+    },
+    {
+      'Which woman was a mathematician whose calculations of orbital mechanics as a NASA employee were critical to the success of the first and subsequent U.S. crewed spaceflights?': [
+        'Katherine Johnson',
+        'Grace Murray Hopper',
+        'Radia Perlman',
+        'Carol Shaw',
+      ],
+    },
+    {
+      'Which was one of the first women to study math, astronomy and philosophy': [
+        'Hypatia',
+        'Margaret Hamilton',
+        'Grace Murray Hopper',
+        'Meg Whitman',
+      ],
+    },
+    {
+      'Who gets credited with having found the first bug?': [
+        'Grace Murray Hopper',
+        'Hedy Lamarr',
+        'Ada Lovelace',
+        'Sister Mary Kenneth Keller',
+      ],
+    },
+  ],
+};
